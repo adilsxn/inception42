d7cbfece60d82fb6207ae598a31ed0ad529b311f
adilsxn <66755632+adilsxn@users.noreply.github.com>
2024-09-26 16:31:56 +0100

    changing mariadb config script

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,24 +5,42 @@
 
 #Check if db exists
 
-if [-d "/run/mysqld"]; 
+if [! -d "/run/mysqld"]; then
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld
 fi
 
-if [-d "/run/mysqld"]
-then
-else
-    chown -R mysql:mysql /var/lib/mysql 
+if [-d "/var/lib/mysql/mysql"]; then
+    chown -R mysql:mysql /var/lib/mysql
+	
+	#start the database
     mysql_install_db --basedir=/usr --datadir=var/lib/mysql --user=mysql --rpm > /dev/null
-
+	
+	tmpfile = `mktemp`
+	if [! -f "$tmpfile"]; then
+		return 1
+	fi
+	
+	cat << EOF > ${tmpfile}
+	 USE mysql;
+	 FLUSH PRIVILEGES;
+	 DELETE FROM mysql.user WHERE User='';
+	 DROP DATABASE IF EXISTS test;
+	 DELETE FROM mysql.db WHERE Db='test';
+	 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	 
+	 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+	 
+	 CREATE DATABASE ${WP_DB_NAME};
+	 CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
+	 GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
+	 
+	 FLUSH PRIVILEGES;
+	 EOF
+		/usr/bin/mysqld/ --user=mysql --bootstrap < ${tmpfile}
 fi
 
-#Import database in the mysql command line
-mysql -u root -p$MYSQL_ROOT_PW $MYSQL_DB < /usr/local/bin/wordpress.sql
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-fi
-
-/etc/init.d/mysql stop
-
-exec "$@"
+exec /usr/bin/mysqld --user=mysql --console
