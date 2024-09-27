742d63eb0fa71817ec804f6318f224e6364bf032
adilsxn <24adilsonnunes@gmail.com>
2024-09-27 16:21:36 +0100

    change: dockerfiles, config_scripts and conf files

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
-
-/etc/init.d/mysql start
-
 #Check if db exists
-
 if [! -d "/run/mysqld"]; then
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld
 fi
 
 if [-d "/var/lib/mysql/mysql"]; then
-    chown -R mysql:mysql /var/lib/mysql
-	
-	#start the database
-    mysql_install_db --basedir=/usr --datadir=var/lib/mysql --user=mysql --rpm > /dev/null
-	
-	tmpfile = `mktemp`
-	if [! -f "$tmpfile"]; then
-		return 1
-	fi
-	
-	cat << EOF > ${tmpfile}
-	 USE mysql;
-	 FLUSH PRIVILEGES;
-	 DELETE FROM mysql.user WHERE User='';
-	 DROP DATABASE IF EXISTS test;
-	 DELETE FROM mysql.db WHERE Db='test';
-	 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-	 
-	 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
-	 
-	 CREATE DATABASE ${WP_DB_NAME};
-	 CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
-	 GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';
-	 
-	 FLUSH PRIVILEGES;
-	 EOF
-		/usr/bin/mysqld/ --user=mysql --bootstrap < ${tmpfile}
+        chown -R mysql:mysql /var/lib/mysql
+
+        #start the database
+        mysql_install_db --basedir=/usr --datadir=var/lib/mysql --user=mysql --rpm > /dev/null
+
+        tmpfile = `mktemp`
+        if [! -f "$tmpfile"]; then
+            return 1
+        fi
+
+        cat << EOF > ${tmpfile}
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PW}';
+
+CREATE DATABASE ${WP_DB_NAME};
+CREATE USER '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PW}';
+GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USR}'@'%' IDENTIFIED BY '${WP_DB_PW}';
+
+FLUSH PRIVILEGES;
+EOF
+        /usr/bin/mysqld/ --user=mysql --bootstrap < ${tmpfile}
+        rm -rf ${tmpfile}
 fi
 
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
--- inception/srcs/requirements/wordpress/tools/wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
+
+while ! mariadb -h${DB_HOST} -u${WP_DB_USR} -p${WP_DB_PW} ${WP_DB_NAME} &>/dev/null; do
+    sleep 3
+done
+
 WP_PATH=/var/www/html/wordpress
 
-#check if wp-config file exists
-if [-f ${WP_PATH}/wp-config.php]
-then
-     echo "Wordpress already exists"
-else
-    wget http://wordpress.org/latest.tar.gz
-    tar xfz latest.tar.gz
-    mv wordpress/* .
-    rm -rf latest.tar.gz
-    rm -rf wordpress
-#Getting env vars 
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    sed -i "s/db_name_here/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    cp wp-config-sample.php wp-config.php
+if [ ! -f ${WP_PATH}/wp-config.php]; then
+    wp-cli.phar cli update --yes --allow root
+    wp-cli.phar core download --allow-root
+    wp-cli.phar config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_PW} --dbhost=${DB_HOST} --path=${WP_PATH} --allow-root
+    wp-cli.phar core install --url=${NGINX_HOST}/wordpress --title=${WP_TITLE} --admin-user=${WP_ADMIN_USR} --admin-password=${WP_ADMIN_PW} --admin-email=${WP_ADMIN_EMAIL} --path${WP_PATH} --allow-root
+    wp-cli.phar user create ${WP_USR} ${WP_USR_EMAIL} --user_pass=${WP_USR_PW} --role=subscriber --display_name=${WP_USR} --porcelain --path=${WP_PATH} --allow-root
+    wp-cli.phar theme install inspiro --path=${WP_PATH} --allow-root
+    wp-cli.phar theme status inspiro --allow-root
+fi
 
-echo "Starting wordpress on port 9000"
-exec /usr/sbin/php-fpm7.4 -F -R
-
+exec /usr/sbin/php-fpm81 -F -R
