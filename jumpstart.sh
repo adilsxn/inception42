b7c417d0bc1af343c138cac7058cce9f74f4c729
adilsxn <24adilsonnunes@gmail.com>
2024-09-28 00:32:40 +0100

    fix: typo on src/srcs

--- jumpstart.sh
+++ jumpstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "s/login/${LOGIN}/g" src/.env
-sed -i "s/login/${LOGIN}/g" src/requirements/nginx/conf/nginx.conf
+sed -i "s/login/${LOGIN}/g" srcs/.env
+sed -i "s/login/${LOGIN}/g" srcs/requirements/nginx/conf/nginx.conf
 
-if ! grep -g ${DOMAIN} "/etc/hosts"; then
+if ! grep -q ${DOMAIN} "/etc/hosts"; then
     echo "127.0.0.1" ${DOMAIN} | sudo tee -a /etc/hosts
 fi
